7a83c520dc48cc83544ed9c9aaaf0b1296843fdb
LutchenkoOleh <160421286+LutchenkoOleh@users.noreply.github.com>
2024-10-10 17:50:06 +0200

    1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contacts/operation";
+import { deleteContact } from "../../redux/contacts/operations";
 import css from "./Contact.module.css"
 
 export default function Contact({ contact }) {
